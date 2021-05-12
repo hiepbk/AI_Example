18e18d4b1c5d223f11255c5e2e0399d85307c452
Hoàng Anh Hiệp <50953949+hiepbk@users.noreply.github.com>
2021-05-12 22:22:50 +0700

    Initial commit

---
+++ Du doan tieu duong/main.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+#%% import
+import numpy as np
+from tensorflow import keras
+from numpy import loadtxt
+from sklearn.model_selection import train_test_split
+
+#%% load data
+dataset = loadtxt('dulieu.csv',delimiter=',')
+
+X = dataset[:,0:8]
+y = dataset[:,8]
+X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, test_size= 0.2)
+X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size = 0.2)
+
+model = keras.Sequential()
+model.add(keras.layers.Dense(16,input_dim=8,activation='relu'))
+model.add(keras.layers.Dense(8,activation='relu'))
+model.add(keras.layers.Dense(1,activation='sigmoid'))
+model.summary()
+
+#%% Compile
+model.compile(optimizer='adam',loss='binary_crossentropy',metrics =['accuracy'])
+model.fit(X_train, y_train, batch_size=8,epochs=100,validation_data=(X_val,y_val))
+#%% save model
+model.save("mymodel.h5")
+
+#%% load model without training
+
+model_loaded = keras.models.load_model('mymodel.h5')
+loss, acc = model_loaded.evaluate(X_test, y_test)
